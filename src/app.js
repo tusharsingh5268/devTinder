c86037309121c5a4e4f4092d5acbf16d71e1fadc
Tushar Singh <tusharsingh5268@gmail.com>
2025-06-09 17:44:39 +0530

    Exploring HTTP methods like GET POST DELETE

--- src/app.js
+++ src/app.js
@@ -1,12 +1,26 @@
 const express = require("express");
 
 const app = express();
-
-app.use("/hello", (req, res) => {
-  res.send("Hello Hello Hello...");
+//From this route we are making GET API call
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Tushar", lastName: "Singh" });
 });
 
-app.use("/test", (req, res) => {
+//From this route we are making POST API call
+app.post("/user", (req, res) => {
+  //code of saving data in database
+  console.log("save Data to the Database");
+  res.send("Data is successfully saved to the database");
+});
+
+//From this route we are making DELETE API call
+app.delete("/user", (req, res) => {
+  //code of deleting data from database
+  res.send("Deleted Successfully");
+});
+
+//From this route we are making api call for every http method
+app.use("/user", (req, res) => {
   res.send("Hello from the server...");
 });
 
