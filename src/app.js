686f6a35ed461d8b511d094500152c155abb2f73
Tushar Singh <tusharsingh5268@gmail.com>
2025-06-12 01:17:04 +0530

    Error handling done

--- src/app.js
+++ src/app.js
@@ -9,13 +9,25 @@ app.get("/user", userAuth, (req, res) => {
 });
 
 app.get("/admin/getData", (req, res) => {
-  console.log("get All the route Handler");
-  res.send("Response for get all data");
+  try {
+    console.log("get All the route Handler");
+    throw Error("sdfsfsd");
+    res.send("Response for get all data");
+  } catch (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.get("/admin/deleteData", (req, res) => {
   console.log("Delete Data route Handler");
   res.send("Delete Data");
+});
+// we can also use Middleware for error handling and always write it at last
+app.use("/", (err, req, res, next) => {
+  console.log("err", err);
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen(3000, () => {
