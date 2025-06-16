fbdb3196cba67ceb273e9e8afed1ff0453d494c8
Tushar Singh <tusharsingh5268@gmail.com>
2025-06-17 01:01:35 +0530

    Delete and Update Done

--- src/app.js
+++ src/app.js
@@ -7,17 +7,6 @@ const app = express();
 //Middleware for coverting readable stream to Javascript object
 app.use(express.json());
 
-//When user login
-app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.send("Data Saved Successfully");
-  } catch (err) {
-    res.status(400).send("Failed to save data");
-  }
-});
-
 //Get data from database based on email id
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
@@ -46,6 +35,41 @@ app.get("/feed", async (req, res) => {
     }
   } catch (err) {
     res.status(400).send("Something went wrong");
+  }
+});
+
+//When user login
+app.post("/signup", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("Data Saved Successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Delete the user by user id
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    console.log("Deleted user id");
+    await User.findByIdAndDelete(userId);
+    res.send("Successfully deleted the User");
+  } catch (err) {
+    res.status(400).send("Failed to save data");
+  }
+});
+
+//Update the user by user id
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    await User.findByIdAndUpdate(userId, data);
+    res.send("Updated Successfully");
+  } catch (err) {
+    res.status(400).send("Failed to save data");
   }
 });
 
